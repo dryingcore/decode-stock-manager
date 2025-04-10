c7bf2340ac096cb8eb246db51f6712547b23c8f2
Gabriel <protonmail.default.sense07@protonmail.com>
2025-04-10 17:12:21 +0000

    Apr 10, 2025, 2:12 PM

--- src/utils/fileManager.ts
+++ src/utils/fileManager.ts
@@ -1,19 +1,17 @@
-import { app } from 'electron';
-import path from "path";
-import fs from "fs";
+import { app } from 'electron'
+import path from 'path'
+import fs from 'fs'
 
-
-const dataPath = path.join(app.getPath('userData'), 'data.json');
-
+const dataPath = path.join(app.getPath('userData'), 'data.json')
 
 export function saveData(data: any) {
-  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), "utf8");
+  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), 'utf8')
 }
 
 export function loadData() {
   if (fs.existsSync(dataPath)) {
-    return JSON.parse(fs.readFileSync(dataPath, "utf8"));
+    return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
   } else {
-    return {};
+    return {}
   }
 }
